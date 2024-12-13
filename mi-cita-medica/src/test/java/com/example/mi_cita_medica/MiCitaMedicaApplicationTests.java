d0706eb282ae59ed3054af71fccdaf5c1e79aa91
Andres <ua072409@gmail.com>
2024-12-13 12:05:11 -0500

    Configuracion inicial del proyecto

---
+++ mi-cita-medica/src/test/java/com/example/mi_cita_medica/MiCitaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mi_cita_medica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiCitaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
