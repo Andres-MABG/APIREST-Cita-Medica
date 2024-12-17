d89d47c7f56fd12b58fb809749fefe971a8e2d93
Andres <ua072409@gmail.com>
2024-12-17 05:33:15 -0500

    Configuracion de escaneo de paquetes

--- mi-cita-medica/src/main/java/com/example/mi_cita_medica/MiCitaMedicaApplication.java
+++ mi-cita-medica/src/main/java/com/example/mi_cita_medica/MiCitaMedicaApplication.java
@@ -2,10 +2,16 @@ package com.example.mi_cita_medica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = "com.example.controladores")
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.repositorios")
+@EntityScan(basePackages = "com.example.entidades")
+@ComponentScan(basePackages = {"com.example.controladores","com.example.config","com.example.dto","com.example.servicios","com.example.repositorios","com.example.entidades","com.example.DTO"})
 @RestController
 public class MiCitaMedicaApplication {
 
